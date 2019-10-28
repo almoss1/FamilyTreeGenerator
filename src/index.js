90332eeefed425f60eeb37b40f86384fe720a5f5
Alex Mosseri <aljmo98@gmail.com>
2019-10-28 17:26:41 -0400

    Updating the form.html

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ app.use(express.static('src/frontend'));
 app.use(express.urlencoded({ extended: false }));
 
 app.post('/my-form-route', (request, response) => {
-  console.log('SOMEONE SUBMITTED A FORM:', request.body);
-  response.redirect('/');
+    console.log('SOMEONE SUBMITTED A FORM:', request.body);
+    response.redirect('/');
 });
 
 app.listen(PORT, () => {
-  console.log(`Running FamTree backend on ${PORT}!`);
+    console.log(`Running FamTree backend on ${PORT}!`);
 });
